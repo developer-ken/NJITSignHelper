11697b2a3db064d2acb03d0710f41e289c5ef90d
Developer_ken <dengbw01@outlook.com>
2021-01-21 22:34:03 +0800

    添加项目文件。

---
+++ NJITSignHelper/PhyLocation/Location.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NJITSignHelper.PhyLocation
+{
+    class Location
+    {
+        public double lat, lon;
+        public string locName;
+
+        public Location(double lat,double lon)
+        {
+            this.lat = lat;
+            this.lon = lon;
+        }
+
+        public static double operator -(Location a, Location b)
+        {
+            return a.DistanceTo(b);
+        }
+
+        /// <summary>
+        /// 返回两点间距离 米
+        /// </summary>
+        /// <param name="target"></param>
+        /// <returns></returns>
+        public double DistanceTo(Location target)
+        {
+            double dLat1InRad = lat * (Math.PI / 180);
+            double dLong1InRad = lon * (Math.PI / 180);
+            double dLat2InRad = target.lat * (Math.PI / 180);
+            double dLong2InRad = target.lon * (Math.PI / 180);
+            double dLongitude = dLong2InRad - dLong1InRad;
+            double dLatitude = dLat2InRad - dLat1InRad;
+            double a = Math.Pow(Math.Sin(dLatitude / 2), 2) + Math.Cos(dLat1InRad) * Math.Cos(dLat2InRad) * Math.Pow(Math.Sin(dLongitude / 2), 2);
+            double c = 2 * Math.Atan2(Math.Sqrt(a), Math.Sqrt(1 - a));
+            double dDistance = EarthRadiusKm * c;
+            return dDistance;
+        }
+
+        public const double EarthRadiusKm = 6378.137; // WGS-84
+    }
+}
---
+++ NJITSignHelper/SignMsgLib/Client.cs
@@ -0,0 +1,136 @@
+﻿using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Security.Cryptography;
+
+namespace NJITSignHelper.SignMsgLib
+{
+    class Client
+    {
+        public const int SCHOOL_CODE = 11276;
+        public const string ACCESS_TOKEN = "5e5b7d74e7b43fc22a851e615fd2792f";
+        public const string APPID = "amp-ios-11276";
+        public const string UA = "Mozilla/5.0 (Linux; Android 10; Mi 10 Build/QKQ1.191117.002; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/83.0.4103.101 Mobile Safari/537.36  cpdaily/8.2.17 wisedu/8.2.17 okhttp/3.12.4";
+
+        public string Cpdaily_Extension;
+        public string MOD_AUTH_CAS;
+        public int StudentNumber;
+
+        public JObject HTTP_POST(string url, JObject payload)
+        {
+            string result = "";
+            HttpWebRequest req = (HttpWebRequest)WebRequest.Create(url);
+            req.Method = "POST";
+            req.ContentType = "application/json";
+
+            req.Headers.Add("accessToken", ACCESS_TOKEN);
+            req.Headers.Add("appId", APPID);
+            req.Headers.Add("Cpdaily-Extension", Cpdaily_Extension);
+            req.CookieContainer = new CookieContainer();
+            req.CookieContainer.Add(new Cookie("MOD_AUTH_CAS", MOD_AUTH_CAS, "/", ".njit.campusphere.net"));
+
+            req.UserAgent = UA;
+
+            byte[] data = Encoding.UTF8.GetBytes(payload.ToString());
+            req.ContentLength = data.Length;
+            using (Stream reqStream = req.GetRequestStream())
+            {
+                reqStream.Write(data, 0, data.Length);
+                reqStream.Close();
+            }
+
+            HttpWebResponse resp = (HttpWebResponse)req.GetResponse();
+            if (resp.ContentType.IndexOf("json") >= 0)
+            {
+                Stream stream = resp.GetResponseStream();
+                using (StreamReader reader = new StreamReader(stream, Encoding.UTF8))
+                {
+                    result = reader.ReadToEnd();
+                }
+                return (JObject)JsonConvert.DeserializeObject(result);
+            }
+            else return null;
+        }
+
+        /// <summary>
+        /// 创建客户端
+        /// </summary>
+        /// <param name="cpe">Cpdaily-Extension的值</param>
+        /// <param name="ticket">MOD_AUTH_CAS的值</param>
+        public Client(string cpe, string ticket, int studentNo)
+        {
+            Cpdaily_Extension = cpe;
+            MOD_AUTH_CAS = ticket;
+            StudentNumber = studentNo;
+        }
+
+        public SignObject[] getSignList(int lastupdate = 0)
+        {
+            /*
+{
+    "userId": "208200611",
+    "schoolCode": "11276",
+    "sign": "95c143160bcb0a3c65e0f2811cd3b2fb",
+    "timestamp": "0",
+    "page": {
+        "start": "0",
+        "size": "200",
+        "total": ""
+    }
+}
+             */
+            List<SignObject> reslist = new List<SignObject>();
+            int start = 0;
+            int step = 200;
+            do
+            {
+                var payload = new JObject();
+                payload.Add("userId", StudentNumber.ToString());
+                payload.Add("schoolCode", SCHOOL_CODE.ToString());
+                payload.Add("sign", MD5Encrypt(ACCESS_TOKEN + SCHOOL_CODE.ToString() + StudentNumber.ToString()));
+                payload.Add("timestamp", lastupdate.ToString());
+
+                var page = new JObject();
+                page.Add("start", start);
+                page.Add("size", step);
+                page.Add("total", "");
+                payload.Add("page", page);
+
+                var result = HTTP_POST("http://messageapi.campusphere.net/message_pocket_web/V2/mp/restful/mobile/message/extend/get", payload);
+                if (result.Value<int>("status") != 200) throw new Exception(result.Value<string>("msg"));
+                try
+                {
+                    if (result["page"] == null || ((JArray)result["msgsNew"]).Count == 0) break;
+                }
+                catch
+                {
+                    break;
+                }
+                foreach (JObject jb in (JArray)result["msgsNew"])
+                {
+                    if (!jb.Value<bool>("isHandled"))//仅处理没有签过的到
+                        reslist.Add(new SignObject(jb, this));
+                }
+                if (result["page"].Value<int>("size") < step) break;
+            } while (true);
+            return reslist.ToArray();
+        }
+
+        public static string MD5Encrypt(string password)
+        {
+            MD5CryptoServiceProvider md5Hasher = new MD5CryptoServiceProvider();
+            byte[] hashedDataBytes;
+            hashedDataBytes = md5Hasher.ComputeHash(Encoding.UTF8.GetBytes(password));
+            StringBuilder tmp = new StringBuilder();
+            foreach (byte i in hashedDataBytes)
+            {
+                tmp.Append(i.ToString("x2"));
+            }
+            return tmp.ToString();
+        }
+    }
+}
